1eb5c9df66aad632ce9b2b4248c3c4c55aa3e062
adeirfanimanda <adeheaven09@gmail.com>
2024-08-02 21:43:32 +0700

    menambahkan route sesuai informasi data

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,63 +5,74 @@
     <div class="row mb-3">
         {{-- menampilkan informasi jumlah data Pengguna --}}
         <div class="col-lg-6 col-xl-3">
-            <div class="bg-white rounded-2 shadow-sm p-4 p-lg-4-2 mb-4">
-                <div class="d-flex align-items-center justify-content-start">
-                    <div class="text-muted me-4">
-                        <i class="ti ti-users fs-1 bg-warning text-white rounded-2 p-2"></i>
-                    </div>
-                    <div>
-                        <p class="text-muted mb-1">Pengguna</p>
-                        {{-- tampilkan data --}}
-                        <h5 class="fw-bold mb-0">{{ $totalUser }}</h5>
+            <a href="{{ route('admin.pengguna.index') }}" class="text-decoration-none">
+                <div class="bg-white rounded-2 shadow-sm p-4 p-lg-4-2 mb-4">
+                    <div class="d-flex align-items-center justify-content-start">
+                        <div class="text-muted me-4">
+                            <i class="ti ti-users fs-1 bg-warning text-white rounded-2 p-2"></i>
+                        </div>
+                        <div>
+                            <p class="text-muted mb-1">Pengguna</p>
+                            {{-- tampilkan data --}}
+                            <h5 class="fw-bold mb-0" style="color: #697A8D;">{{ $totalUser }}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
+
         {{-- menampilkan informasi jumlah data Kategori --}}
         <div class="col-lg-6 col-xl-3">
-            <div class="bg-white rounded-2 shadow-sm p-4 mb-4">
-                <div class="d-flex align-items-center justify-content-start">
-                    <div class="me-4">
-                        <i class="ti ti-category fs-1 bg-primary-2 text-white rounded-2 p-2"></i>
-                    </div>
-                    <div>
-                        <p class="text-muted mb-1">Kategori</p>
-                        {{-- tampilkan data --}}
-                        <h5 class="fw-bold mb-0">{{ $totalCategory }}</h5>
+            <a href="{{ route('categories.index') }}" class="text-decoration-none">
+                <div class="bg-white rounded-2 shadow-sm p-4 mb-4">
+                    <div class="d-flex align-items-center justify-content-start">
+                        <div class="me-4">
+                            <i class="ti ti-category fs-1 bg-primary-2 text-white rounded-2 p-2"></i>
+                        </div>
+                        <div>
+                            <p class="text-muted mb-1">Kategori</p>
+                            {{-- tampilkan data --}}
+                            <h5 class="fw-bold mb-0" style="color: #697A8D;">{{ $totalCategory }}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
+
         {{-- menampilkan informasi jumlah data Product --}}
         <div class="col-lg-6 col-xl-3">
-            <div class="bg-white rounded-2 shadow-sm p-4 p-lg-4-2 mb-4">
-                <div class="d-flex align-items-center justify-content-start">
-                    <div class="me-4">
-                        <i class="ti ti-copy fs-1 bg-success text-white rounded-2 p-2"></i>
-                    </div>
-                    <div>
-                        <p class="text-muted mb-1">Obat</p>
-                        {{-- tampilkan data --}}
-                        <h5 class="fw-bold mb-0">{{ $totalProduct }}</h5>
+            <a href="{{ route('products.index') }}" class="text-decoration-none">
+                <div class="bg-white rounded-2 shadow-sm p-4 p-lg-4-2 mb-4">
+                    <div class="d-flex align-items-center justify-content-start">
+                        <div class="me-4">
+                            <i class="ti ti-copy fs-1 bg-success text-white rounded-2 p-2"></i>
+                        </div>
+                        <div>
+                            <p class="text-muted mb-1">Obat</p>
+                            {{-- tampilkan data --}}
+                            <h5 class="fw-bold mb-0" style="color: #697A8D;">{{ $totalProduct }}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
+
         {{-- menampilkan informasi jumlah data Transaction --}}
         <div class="col-lg-6 col-xl-3">
-            <div class="bg-white rounded-2 shadow-sm p-4 p-lg-4-2 mb-4">
-                <div class="d-flex align-items-center justify-content-start">
-                    <div class="text-muted me-4">
-                        <i class="ti ti-folders fs-1 bg-info text-white rounded-2 p-2"></i>
-                    </div>
-                    <div>
-                        <p class="text-muted mb-1">Transaksi</p>
-                        {{-- tampilkan data --}}
-                        <h5 class="fw-bold mb-0">{{ $totalTransaction }}</h5>
+            <a href="{{ route('transactions.index') }}" class="text-decoration-none">
+                <div class="bg-white rounded-2 shadow-sm p-4 p-lg-4-2 mb-4">
+                    <div class="d-flex align-items-center justify-content-start">
+                        <div class="text-muted me-4">
+                            <i class="ti ti-folders fs-1 bg-info text-white rounded-2 p-2"></i>
+                        </div>
+                        <div>
+                            <p class="text-muted mb-1">Transaksi</p>
+                            {{-- tampilkan data --}}
+                            <h5 class="fw-bold mb-0" style="color: #697A8D;">{{ $totalTransaction }}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 
